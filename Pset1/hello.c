0e49dffa654a99d02898d33a395b1627e3a7b921
Tkota10 <68127292+Tkota10@users.noreply.github.com>
2023-05-17 21:30:31 -0500

    Create hello.c

---
+++ Pset1/hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef char* string;
+
+int main(int argc, string argv[] ) {
+
+   char* exe_name = argv[1]; //Initialized exe_name to give me a mechanism of checking the command line arguments.
+   char str[129]; //Initialized str to consider up 129 characters.
+   str[0] = 5; //Intialize the string to be a random number
+   if (argc > 1){ //this is to consider the case of where this is more than 1 command line argument.
+      printf("Hello, %s\n", exe_name);
+   } else {
+   
+   printf( "What is your name? ");
+   scanf("%129[^\n]",str); //Getsthe maximum input of 129
+  
+   if (str[0] == 5){ //If the user inputs nothing (return line), than the program finishes.
+      return -1;
+   }
+   printf( "Hello, "); //all other inputs.
+   puts( str );
+}
+   
+   return 0; //ends program
+}
